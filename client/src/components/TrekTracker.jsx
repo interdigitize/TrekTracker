cbe7b8cf9559d77e9136d06106a7664db30cdc9f
interdigitize <interdigitize@users.noreply.github.com>
2017-08-27 19:00:58 -0700

    Add component to render navbar logo

---
+++ client/src/components/TrekTracker.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TrekTracker = (props) => (
+  <div>
+    <img src="http://i.imgur.com/VVS3nLS.png" style={{maxWidth: '65px', paddingRight: '7px'}}/> Trek Tracker
+  </div>
+);
+
+export default TrekTracker;
